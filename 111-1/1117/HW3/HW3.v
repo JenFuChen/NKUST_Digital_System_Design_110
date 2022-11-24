c4863710977d741e461bcbf632358742cbe6a1f0
Chen Jenfu <dht98010@gmail.com>
2022-11-24 18:11:49 +0800

    0

--- 111-1/1117/HW3/HW3.v
+++ 111-1/1117/HW3/HW3.v
@@ -1,83 +1,230 @@
-module HW3(SW, HEX0, HEX1, HEX2, HEX3, HEX4, HEX5, HEX6, HEX7);
-	input [18:0] SW;
-	output [6:0] HEX0,HEX1,HEX2,HEX3,HEX4,HEX5,HEX6,HEX7;
-
-	reg [7:0] A, B;
-	reg [15:0] result;
-
-	parameter flag = 1;
-	// Display current num
-	// A
-	LED_Decoder U_1(.data(SW[17:14]), .mode(1), .out(HEX7));
-	LED_Decoder U_2(.data(SW[13:10]), .mode(1), .out(HEX6));
-	// B
-	LED_Decoder U_3(.data(SW[9:6]), .mode(1), .out(HEX5));
-	LED_Decoder U_4(.data(SW[5:2]), .mode(1), .out(HEX4));
-
-	// Result Display
-	LED_Decoder U_5(.data(result[15:12]), .mode(flag), .out(HEX3));
-	LED_Decoder U_6(.data(result[11:8]), .mode(flag), .out(HEX2));
-	LED_Decoder U_7(.data(result[7:4]), .mode(flag), .out(HEX1));
-	LED_Decoder U_8(.data(result[3:0]), .mode(flag), .out(HEX0));
-
-	assign A = SW[17:10];
-	assign B = SW[9:20];
-
-	always @(*) begin
-		if(SW[17:14] > 4'b1011 || SW[13:10] > 4'b1011 || SW[9:6] > 4'b1011 || SW[5:2] > 4'b1011)begin
-			flag = 0
-			result = 16'b0000_0001_0010_0011;// UndF
-		end
-		else begin
-			flag = 1;
-			case (SW[1:0])
-				2'b00: result = A + B;
-				2'b01: result = A - B;
-				2'b10: result = A * B;
-				2'b11: result = A / B;
-			endcase
-		end
-	end
+module HW3(SW,HEX0,HEX1,HEX2,HEX3,HEX4,HEX5,HEX6,HEX7);
+input [17:0]SW;
+output [6:0]HEX0,HEX1,HEX2,HEX3,HEX4,HEX5,HEX6,HEX7;
+reg [7:0]A,B;
+reg [15:0]C,D;
+integer i;
+wire[6:0]hex0,hex1,hex2,hex3,hex4,hex5,hex6,hex7;
+reg [6:0]HEX0,HEX1,HEX2,HEX3,HEX4,HEX5,HEX6,HEX7;
+reg [6:0]H0,H1,H2,H3,H4,H5,H6,H7;
+LED_Decoder L0(SW[17:14], hex7);
+LED_Decoder L1(SW[13:10], hex6);
+LED_Decoder L2(SW[9:6], hex5);
+LED_Decoder L3(SW[5:2], hex4);
+LED_Decoder L4(D[15:12], hex3);
+LED_Decoder L5(D[11:8], hex2);
+LED_Decoder L6(D[7:4], hex1);
+LED_Decoder L7(D[3:0], hex0);
+always@(*)begin
+HEX7=hex7;
+HEX6=hex6;
+HEX5=hex5;
+HEX4=hex4;
+end
+always@(*)begin
+  A=SW[17:14]*10+SW[13:10];
+  B=SW[9:6]*10+SW[5:2];
+  case(SW[1:0])
+   2'h0: begin
+     if(SW[17:14]>4'd9 || SW[13:10]>4'd9 || SW[9:6]>4'd9 || SW[5:2]>4'd9)begin
+      HEX1=~(7'b0110111);
+      HEX2=~(7'b1110111);
+      HEX3=~(7'b0110111);
+      HEX0=~(7'b0000000);
+     end
+     else begin
+      C=A+B;
+      if(C<10)begin
+       D[3:0]=C;
+       D[7:4]=4'h0;
+       D[11:8]=4'h0;
+       D[15:12]=4'h0;
+      end
+      else if(C>9 && C < 100)begin
+       D[3:0]=C %10;
+       D[7:4]=C/10;
+       D[11:8]=4'h0;
+       D[15:12]=4'h0;
+      end
+      else if(C>99 && C < 1000)begin
+       D[3:0]=C %10;
+       C=C/10;
+       D[7:4]=C%10;
+       D[11:8]=C/10;
+       D[15:12]=4'h0;
+      end
+      else begin
+       D[3:0]=C %10;
+       C=C/10;
+       D[7:4]=C%10;
+       C=C/10;
+       D[11:8]=C%10;
+       D[15:12]=C/10;
+      end
+      HEX3=hex3;
+      HEX2=hex2;
+      HEX1=hex1;
+      HEX0=hex0;
+     end
+     end
+   2'h1:begin
+     if(SW[17:14]>4'd9 || SW[13:10]>4'd9 || SW[9:6]>4'd9 || SW[5:2]>4'd9)begin
+      HEX1=~(7'b0110111);
+      HEX2=~(7'b1110111);
+      HEX3=~(7'b0110111);
+      HEX0=~(7'b0000000);
+     end
+     else begin
+      {HEX3,HEX2,HEX1,HEX0}=~28'h0;
+      C=A-B;
+      if(C<10)begin
+       D[3:0]=C;
+       D[7:4]=4'h0;
+       D[11:8]=4'h0;
+       D[15:12]=4'h0;
+      end
+      else if(C>9 && C < 100)begin
+       D[3:0]=C %10;
+       D[7:4]=C/10;
+       D[11:8]=4'h0;
+       D[15:12]=4'h0;
+      end
+      else if(C>99 && C < 1000)begin
+       D[3:0]=C %10;
+       C=C/10;
+       D[7:4]=C%10;
+       D[11:8]=C/10;
+       D[15:12]=4'h0;
+      end
+      else begin
+       D[3:0]=C %10;
+       C=C/10;
+       D[7:4]=C%10;
+       C=C/10;
+       D[11:8]=C%10;
+       D[15:12]=C/10;
+      end
+      HEX3=hex3;
+      HEX2=hex2;
+      HEX1=hex1;
+      HEX0=hex0;
+      end
+     end
+   2'h2:begin
+     if(SW[17:14]>4'd9 || SW[13:10]>4'd9 || SW[9:6]>4'd9 || SW[5:2]>4'd9)begin
+      HEX1=~(7'b1110110);
+      HEX2=~(7'b1110111);
+      HEX3=~(7'b1110110);
+      HEX0=~(7'b0000000);
+     end
+     else begin
+      {HEX3,HEX2,HEX1,HEX0}=~28'h0;
+      C=A*B;
+      if(C<10)begin
+       D[3:0]=C;
+       D[7:4]=4'h0;
+       D[11:8]=4'h0;
+       D[15:12]=4'h0;
+      end
+      else if(C>9 && C < 100)begin
+       D[3:0]=C %10;
+       D[7:4]=C/10;
+       D[11:8]=4'h0;
+       D[15:12]=4'h0;
+      end
+      else if(C>99 && C < 1000)begin
+       D[3:0]=C %10;
+       C=C/10;
+       D[7:4]=C%10;
+       D[11:8]=C/10;
+       D[15:12]=4'h0;
+      end
+      else begin
+       D[3:0]=C %10;
+       C=C/10;
+       D[7:4]=C%10;
+       C=C/10;
+       D[11:8]=C%10;
+       D[15:12]=C/10;
+      end
+      HEX3=hex3;
+      HEX2=hex2;
+      HEX1=hex1;
+      HEX0=hex0;
+      end
+     end
+   2'h3: begin
+     if(B==4'h0)begin
+     {HEX3,HEX2,HEX1,HEX0} = ~28'b0111110_0110111_1011110_1110001;
+     end
+     else if(SW[17:14]>4'd9 || SW[13:10]>4'd9 || SW[9:6]>4'd9 || SW[5:2]>4'd9)begin
+      HEX1=~(7'b0110111);
+      HEX2=~(7'b1110111);
+      HEX3=~(7'b0110111);
+      HEX0=~(7'b0000000);
+     end
+     else begin
+      {HEX3,HEX2,HEX1,HEX0}=~28'h0;
+      C=A/B;
+       if(C<10)begin
+       D[3:0]=C;
+       D[7:4]=4'h0;
+       D[11:8]=4'h0;
+       D[15:12]=4'h0;
+      end
+      else if(C>9 && C < 100)begin
+       D[3:0]=C %10;
+       D[7:4]=C/10;
+       D[11:8]=4'h0;
+       D[15:12]=4'h0;
+      end
+      else if(C>99 && C < 1000)begin
+       D[3:0]=C %10;
+       C=C/10;
+       D[7:4]=C%10;
+       D[11:8]=C/10;
+       D[15:12]=4'h0;
+      end
+      else begin
+       D[3:0]=C %10;
+       C=C/10;
+       D[7:4]=C%10;
+       C=C/10;
+       D[11:8]=C%10;
+       D[15:12]=C/10;
+      end
+      HEX3=hex3;
+      HEX2=hex2;
+      HEX1=hex1;
+      HEX0=hex0;
+     end
+    end
+  endcase 
+  
+end
 endmodule
-
-module LED_Decoder(data, mode, out);
-	input [3:0] data;
-	input mode;
-	output reg[6:0] out;
-
-	always@(*)begin
-		if(mode == 1)begin
-			case(data)
-				0 : out <= ~7'b0111111;
-				1 : out <= ~7'b0000110; 
-				2 : out <= ~7'b1011011;
-				3 : out <= ~7'b1001111;
-				4 : out <= ~7'b1100110;
-				5 : out <= ~7'b1101101;
-				6 : out <= ~7'b1111101;
-				7 : out <= ~7'b0000111;
-				8 : out <= ~7'b1111111;
-				9 : out <= ~7'b1101111;
-				10: out <= ~7'b1110111;
-				11: out <= ~7'b1111100;
-				12: out <= ~7'b1011000;
-				13: out <= ~7'b1011110;
-				14: out <= ~7'b1111001;
-				15: out <= ~7'b1110001;
-			endcase
-		end
-		else begin
-			case(data)
-				0 : out <= ~7'b0111110; // U
-				1 : out <= ~7'b1010100; // n
-				2 : out <= ~7'b1011110; // d
-				3 : out <= ~7'b1110001; // F
-			endcase
-		end
-
-		
-	end
-
-endmodule
-
-// {HEX3,HEX2,HEX1,HEX0} = 28'b0111110_1010100_1011110_1110001; UndF
+module LED_Decoder(data, out);
+ input [3:0] data;
+ output reg [6:0] out;
+ always@(*)begin
+  case(data)
+   0 : out <= ~7'b0111111;
+   1 : out <= ~7'b0000110; 
+   2 : out <= ~7'b1011011;
+   3 : out <= ~7'b1001111;
+   4 : out <= ~7'b1100110;
+   5 : out <= ~7'b1101101;
+   6 : out <= ~7'b1111101;
+   7 : out <= ~7'b0000111;
+   8 : out <= ~7'b1111111;
+   9 : out <= ~7'b1101111;
+   10: out <= ~7'b1110111;
+   11: out <= ~7'b1111100;
+   12: out <= ~7'b1011000;
+   13: out <= ~7'b1011110;
+   14: out <= ~7'b1111001;
+   15: out <= ~7'b1110001;
+  endcase
+ end
+ endmodule
+// 指撥開關+-*-
--- 111-1/1124/HW1/HW1.v
+++ 111-1/1124/HW1/HW1.v
@@ -1,64 +1,72 @@
 module HW1(clk, SW, KEY, LEDR);
-    input clk;
-    input [17:0] SW;
-    input [3:0] KEY;
-    output reg [17:0] LEDR;
-	 
-    reg [17:0] counter=0, counter2=0, count=0;
-    reg _KEY1, _KEY3;
-	 
-	reg clk5 = 0;
-	/// make CLK to  5HZ
-    always @(posedge clk) begin
-        if(counter2 >= 5_000_000)begin
-            counter2 <= 1;
-            clk5 <= ~clk5;
-        end else begin
-            counter2 <= counter2 + 1;
-        end
-    end
-	 
-	 /// Clean Button
-    always @(posedge clk) begin
-        if(count >= 1000)begin
-            count <= 0;
-            _KEY1 <= KEY[1];
-            _KEY3 <= KEY[3];
-        end else if(_KEY1 ^ KEY[1] || _KEY3 ^ KEY[3]) count <= count + 1;
-        else count <= 0;
-    end
-
-	 /// Work
-    always @(posedge clk5) begin
-		 if(!_KEY1)begin
-			counter <= 18'b11_1110_0000_0000_0000;
-			LEDR <= ~18'b11_1111_1111_1111_1111;
-		 end
-       else begin
-            if(_KEY3 == 1) begin
-                LEDR <= ~18'b000000000000000000;
-                
-            end
-            else begin
-
-            end
-       end
+ input clk;
+   input [17:0] SW;
+   input [3:0] KEY;
+   output reg [17:0] LEDR;
+ reg [31:0] counter=0, counter2=0, count=0;
+ reg _KEY1, _KEY3;
+ reg clk5 = 0;
+ 
+ /// make CLK to  5HZ
+ always @(posedge clk) begin
+   if(counter2 >= 5_000_000)begin
+   counter2 <= 1;
+   clk5 <= ~clk5;
+   end else begin
+   counter2 <= counter2 + 1;
+   end
+ end
+ 
+ reg flag = 0, rst = 0;
+ reg [10:0] switch = 1;
+ /// Clean Button
+ always @(posedge clk) begin
+  if(count >= 1000)begin
+   count <= 0;
+   _KEY1 <= KEY[1];
+   _KEY3 <= KEY[3];
+  end else if(_KEY1 ^ KEY[1] || _KEY3 ^ KEY[3]) 
+   count <= count + 1;
+  else 
+   count <= 0;
+ end
+ reg [17:0] result;
+ always @(negedge _KEY1) begin
+  rst = ~rst;
+ end
+ 
+ always @(negedge _KEY3) begin
+  flag = ~flag;
+ end
+ 
+ /// Work
+   always @(posedge clk5) begin
+  if(rst)begin
+   switch = 2;
+   counter <= 18'b11_1110_0000_0000_0000;
+   LEDR <= ~18'b11_1111_1111_1111_1111;
+  end
+      else begin
+   if(flag == 1) begin
+     case(SW[17:16])
+     2'b00:begin
+      LEDR <= LEDR >> 1;
+     end
+     2'b01:begin
+      LEDR <= ~LEDR;
+     end
+     2'b10:begin
+      LEDR <= LEDR + 2;
+     end
+     2'b11:begin
+      LEDR <= LEDR << 1;
+     end
+    endcase
+   end
+         else begin
+     
+         end
+      end
     end
 endmodule
-// case(SW[17:16])
-//     2'b00:begin
-//         LEDR <= ~ (18'b10_1010_1010_1010_1010);
-//         end
-//         2'b01:begin
-//         if(counter != 0)
-//             LEDR <= (counter >> 1);
-//         else 
-//             counter <= 18'b11_1110_0000_0000_0000;
-//         end
-//         2'b10:begin
-
-//         end
-//         2'b11:begin
-
-//         end
-// endcase
+跑馬燈
