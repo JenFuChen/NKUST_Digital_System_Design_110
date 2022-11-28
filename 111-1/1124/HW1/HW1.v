457ef46fe8fef010a376f13a3d89ba670cfade69
Chen Jenfu <dht98010@gmail.com>
2022-11-28 08:20:02 +0800

    0

--- 111-1/1124/HW1/HW1.v
+++ 111-1/1124/HW1/HW1.v
@@ -1,12 +1,13 @@
 module HW1(clk, SW, KEY, LEDR);
- input clk;
-   input [17:0] SW;
-   input [3:0] KEY;
-   output reg [17:0] LEDR;
- reg [31:0] counter=0, counter2=0, count=0;
- reg _KEY1, _KEY3;
- reg clk5 = 0;
- 
+    input clk;
+    input [17:0] SW;
+    input [3:0] KEY;
+    output reg [17:0] LEDR;
+    
+    reg _KEY1, _KEY3;
+    reg clk5 = 0;
+
+    reg [31:0] counter=0, counter2=0, count=0;
  /// make CLK to  5HZ
  always @(posedge clk) begin
    if(counter2 >= 5_000_000)begin
@@ -20,15 +21,16 @@ module HW1(clk, SW, KEY, LEDR);
  reg flag = 0, rst = 0;
  reg [10:0] switch = 1;
  /// Clean Button
+ 
  always @(posedge clk) begin
-  if(count >= 1000)begin
-   count <= 0;
-   _KEY1 <= KEY[1];
-   _KEY3 <= KEY[3];
-  end else if(_KEY1 ^ KEY[1] || _KEY3 ^ KEY[3]) 
-   count <= count + 1;
-  else 
-   count <= 0;
+    if(count >= 1000)begin
+        count <= 0;
+        _KEY1 <= KEY[1];
+        _KEY3 <= KEY[3];
+    end else if(_KEY1 ^ KEY[1] || _KEY3 ^ KEY[3]) 
+        count <= count + 1;
+    else 
+        count <= 0;
  end
  reg [17:0] result;
  always @(negedge _KEY1) begin
--- 111-1/1124/HW3/HW3.v
+++ 111-1/1124/HW3/HW3.v
@@ -1,6 +1,6 @@
 // ===== Home Work 3 : 1A2B Game ===== //
 
-`define MAXHZ 5_000_000 // to 5 Hz
+`define MAXHZ 5_000_000 // to 5 Hz : 
 
 module HW3(CLK, KEY, SW, HEX0, HEX1, HEX2, HEX3, HEX4, HEX5, HEX6, HEX7);
 
@@ -18,9 +18,9 @@ module HW3(CLK, KEY, SW, HEX0, HEX1, HEX2, HEX3, HEX4, HEX5, HEX6, HEX7);
 	reg 		[2:0] 	flagA, flagB;
 	
 	// Show N A N B
-	LED_Decoder U0(.data(A), 			.mode(mode), .out(HEX7));	// num
+	LED_Decoder U0(.data(A), 		.mode(mode), .out(HEX7));	// num
 	LED_Decoder U1(.data(4'b1010), 	.mode(mode), .out(HEX6));	// A
-	LED_Decoder U2(.data(B), 			.mode(mode), .out(HEX5));	// num
+	LED_Decoder U2(.data(B), 		.mode(mode), .out(HEX5));	// num
 	LED_Decoder U3(.data(4'b1011),	.mode(mode), .out(HEX4));	// B
 	
 	// show num
@@ -71,33 +71,34 @@ module HW3(CLK, KEY, SW, HEX0, HEX1, HEX2, HEX3, HEX4, HEX5, HEX6, HEX7);
 	// ===== Reset ===== //
 	always @(*)begin
 		if(!rst)begin
-			mode 		<= 0;
-			guess		<= 0;
+			mode 	<= 0;
+			guess	<= 0;
 			answer 	<= 0;
 			flagA 	<= 0;
 			flagB 	<= 0;
-			A			<= 0;
-			B 			<= 0;
+			A		<= 0;
+			B 		<= 0;
+			flag 	<= 0;
 		end		
 	end
 	
 	// ===== Start Work ===== //
 	always @(*)begin
-		if( flag && ~(|(SW)) )begin
+		if( !flag  )begin
 			answer 	= SW[15:0];
 			out		= answer;
 		end else begin
-			if( !flag )begin
+			if( flag && ~(|(SW)))begin
 				guess[15:0] = SW[15:0];
 				// Judeg A
-				A = (guess[15:12] == answer[15:12]) ? A + 1 : A;
+				A = (guess[15:12] 	== answer[15:12]) ? A + 1 : A;
 				A = (guess[11:8] 	== answer[11:8] ) ? A + 1 : A;
 				A = (guess[7:4] 	== answer[7:4]  ) ? A + 1 : A;
 				A = (guess[3:0] 	== answer[3:0]  ) ? A + 1 : A;
 				
 				// Judge B
-				B = (guess[15:12] == answer[11:8] 	|| guess[15:12] 	== answer[7:4] 	|| guess[15:12]	== answer[3:0] ) ? B + 1 : B;
-				B = (guess[11:8] 	== answer[15:12] 	|| guess[11:8] 	== answer[7:4] 	|| guess[11:8] 	== answer[3:0] ) ? B + 1 : B;
+				B = (guess[15:12] 	== answer[11:8] 	|| guess[15:12] 	== answer[7:4] 		|| guess[15:12]		== answer[3:0] ) ? B + 1 : B;
+				B = (guess[11:8] 	== answer[15:12] 	|| guess[11:8] 		== answer[7:4] 		|| guess[11:8] 		== answer[3:0] ) ? B + 1 : B;
 				B = (guess[7:4] 	== answer[15:12] 	|| guess[7:4] 		== answer[11:8] 	|| guess[7:4] 		== answer[3:0] ) ? B + 1 : B;
 				B = (guess[3:0] 	== answer[15:12] 	|| guess[3:0] 		== answer[11:8] 	|| guess[3:0] 		== answer[7:4] ) ? B + 1 : B;
 				
@@ -129,32 +130,24 @@ module LED_Decoder(data, out, mode);
 				7 : out <= ~7'b0000111;
 				8 : out <= ~7'b1111111;
 				9 : out <= ~7'b1101111;
-				10: out <= ~7'b1110111;// A
-				11: out <= ~7'b1111100;// b
-				12: out <= ~7'b1011000;// c
-				13: out <= ~7'b1011110;// d
-				14: out <= ~7'b1111001;// E
-				15: out <= ~7'b1110001;// F
+				10: out <= ~7'b1110111; // A
+				11: out <= ~7'b1111100; // b
+				12: out <= ~7'b1011000; // c
+				13: out <= ~7'b1011110; // d
+				14: out <= ~7'b1111001; // E
+				15: out <= ~7'b1110001; // F
 			endcase
 		end
-		else if(mode == 3) begin
+		else if(mode == 3) begin // PASS of LOSE
 			case(data)
-				0 : out <= ~7'b0111111;
-				1 : out <= ~7'b0000110; 
-				2 : out <= ~7'b1011011;
-				3 : out <= ~7'b1001111;
-				4 : out <= ~7'b1100110;
-				5 : out <= ~7'b1101101;
-				6 : out <= ~7'b1111101;
-				7 : out <= ~7'b0000111;
-				8 : out <= ~7'b1111111;
-				9 : out <= ~7'b1101111;
-				10: out <= ~7'b1110111;// A
-				11: out <= ~7'b1111100;// b
-				12: out <= ~7'b1011000;// c
-				13: out <= ~7'b1011110;// d
-				14: out <= ~7'b1111001;// E
-				15: out <= ~7'b1110001;// F
+				0 : out <= ~7'b1110011; // P
+				1 : out <= ~7'b1110111; // A
+				2 : out <= ~7'b1101101; // S
+
+				3 : out <= ~7'b0111111; // O
+				4 : out <= ~7'b0111000; // L
+				5 : out <= ~7'b1111001; // E
+				default : out <= ~7'b0000000; // Waiting Mode
 			endcase
 		end
 	end
