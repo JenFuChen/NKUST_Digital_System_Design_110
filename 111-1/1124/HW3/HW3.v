706a689903c2a91d2867b3810f224b895851acc1
JenFu <dht98010@gmail.com>
2022-12-01 16:28:16 +0800

    0

--- 111-1/1124/HW3/HW3.v
+++ 111-1/1124/HW3/HW3.v
@@ -1,35 +1,39 @@
 // ===== Home Work 3 : 1A2B Game ===== //
 
 `define MAXHZ 5_000_000 // to 5 Hz :  
+`define 
 
-module HW3(CLK_50, KEY, SW, HEX0, HEX1, HEX2, HEX3, HEX4, HEX5, HEX6, HEX7, LEDR , LEDG);
-	input 	CLK_50;
-	wire 	CLK;
-	assign 	CLK = CLK_50;
-	//input     CLK;
+module HW3(clk, KEY, SW, HEX0, HEX1, HEX2, HEX3, HEX4, HEX5, HEX6, HEX7, LEDR , LEDG);
+	//input 	CLK_50;
+	//wire 	CLK;
+	wire 		CLK;
+	assign 	CLK = clk;
+	input    			clk;
 	input  	[3:0]  	KEY;
-	input   [17:0]  SW;
-	output  [6:0]   HEX0,HEX1,HEX2,HEX3,HEX4,HEX5,HEX6,HEX7;
+	input 	[17:0] 	SW;
 	
-	output	reg 	[17:0]  LEDR;
+	output 	[6:0]   	HEX0,HEX1,HEX2,HEX3,HEX4,HEX5,HEX6,HEX7;
+	output	reg [17:0]  LEDR;
 
-	reg   	[15:0]	guess, answer, out;
-	reg   	[31:0]  count, times;
+	reg   	[17:0]	guess, answer, out;
+	reg   	[31:0]  	count, times;
 	reg   	[3:0]  	A, B, showA, showB, run;
-	reg   	[1:0]	State, NextState;
-	reg    	flag = 0, rst = 0;
-	reg   	CLK_5HZ;
+	reg   	[1:0]		State, NextState;
+	// reg    				flag = 0, rst = 0;
+	reg   				CLK_5HZ;
+	output 	[7:0] 	LEDG;
 	
-	output 	[1:0] 	LEDG;
-	assign 			LEDG = State;
-	wire      		_KEY1, _KEY3;
-	
+	assign 				LEDG[1:0] = State;
+	assign 				LEDG[6] = _KEY3;
+	assign 				LEDG[2] = _KEY1;
+	wire      			_KEY1, _KEY3;
+	reg 					flag;
 	parameter    Set = 2'b00, Waiting = 2'b01, Play = 2'b10, SEnd = 2'b11 ;
 	
 	// Show N A N B in State Play, show Run Circle in State Waiting
-	LED_Decoder U0(.data(A),   		.mode(State), .out(HEX7)); // num of A
-	LED_Decoder U1(.data(showA),  	.mode(State), .out(HEX6)); // show A
-	LED_Decoder U2(.data(B),   		.mode(State), .out(HEX5)); // num of B
+	LED_Decoder U0(.data(A),   	.mode(State), .out(HEX7)); // num of A
+	LED_Decoder U1(.data(showA),  .mode(State), .out(HEX6)); // show A
+	LED_Decoder U2(.data(B),   	.mode(State), .out(HEX5)); // num of B
 	LED_Decoder U3(.data(showB), 	.mode(State), .out(HEX4)); // show B
 	
 	// show num ( PASS, LOSE, Answer Num, Guess Num)
@@ -44,13 +48,13 @@ module HW3(CLK_50, KEY, SW, HEX0, HEX1, HEX2, HEX3, HEX4, HEX5, HEX6, HEX7, LEDR
 	Debounce U9(.clk(CLK), .in(KEY[3]), .out(_KEY3));
 	
 	// Make CLK to 5HZ
-	always @(posedge CLK) begin 
-	if( count >= `MAXHZ )begin  
-	count <= 1;
-	CLK_5HZ <= ~ CLK_5HZ;   
-	end else begin
-	count <= count + 1;
-	end
+	always @(posedge CLK) begin
+		if( count >= `MAXHZ )begin
+			count <= 1;
+			CLK_5HZ <= ~ CLK_5HZ;   
+		end else begin
+			count <= count + 1;
+		end
 	end
  
 	// ===== To do When press button ====== //
@@ -59,7 +63,7 @@ module HW3(CLK_50, KEY, SW, HEX0, HEX1, HEX2, HEX3, HEX4, HEX5, HEX6, HEX7, LEDR
 	
 	// Run Circle
 	always @(posedge CLK_5HZ)begin
-		if(State == Waiting)begin
+		if(State == Waiting || State == SEnd)begin
 			if(run < 5)
 				run <= run + 1;
 			else 
@@ -71,7 +75,7 @@ module HW3(CLK_50, KEY, SW, HEX0, HEX1, HEX2, HEX3, HEX4, HEX5, HEX6, HEX7, LEDR
  
  
 	// ===== Reset ===== //
-	always @(negedge _KEY1)begin
+	always @(negedge _KEY1, posedge CLK_5HZ)begin
 		if(!_KEY1)begin
 			State  <= Set;
 		end else begin
@@ -84,57 +88,65 @@ module HW3(CLK_50, KEY, SW, HEX0, HEX1, HEX2, HEX3, HEX4, HEX5, HEX6, HEX7, LEDR
 	always @(negedge _KEY3) begin
 		case (State)
 			Set:begin
+				LEDR <= 17'b10101010101010101;
+				times = 0;
+			end
+			Waiting:begin			
+				times = 0;
+				LEDR <= 0 ;
 				LEDR <= 0;
-			end
-			Waiting:begin
-			end
+				end
 			Play:begin
 				times = times +1;
-				LEDR <=  (LEDR << 1 )+1;
+				LEDR	= (LEDR << 1 ) + 1 ;
 			end
 			SEnd:begin
-
+				times = 0;
+				LEDR = 18'b101010101010101010;
 			end
 		endcase
 	end
 	
  
 	// Change State
-	always @(*) begin
-		if(!_KEY1)
+	always @(posedge CLK_5HZ,negedge _KEY1) begin
+		if(!_KEY1)begin
+			//LEDR <= 17'b10101010101010101;
 			NextState <= Set;
+		end
 		else begin
 			case (State)
+				
 				Set:begin
-					if(!_KEY1) NextState <= Set;
-					else if(!_KEY3)begin 
-					NextState <= Waiting;
-				end
+					if(!_KEY3)begin 
+      				NextState <= Waiting;
+     				end
 				end
 				Waiting:begin
 					if(|SW)
 						NextState <= Waiting;
-					else
+					else if(!_KEY3)
 						NextState <= Play;
+					//LEDR <=  SW;;//(LEDR << 1 )+1;
 				end
 				Play:begin
-					if(A != 4 && times <= 18)
-						NextState <= Play;
-					else 
+					if(guess == answer && !_KEY3)  NextState <= SEnd;
+					if(times >= 17)begin
 						NextState <= SEnd;
+					end
 				end
 				SEnd:begin
-					if(|SW) 
-						NextState <= SEnd;
-					else
+					if(!_KEY3 && !(|SW))
 						NextState <= Set;
+					else
+						NextState <= SEnd;
 				end
 			endcase
 		end
 	end
 		
 	// Output
-	always @(*) begin
+	always @(posedge CLK_5HZ) begin
 		case (State)
 		Set:begin
 			out  = answer;
@@ -146,7 +158,7 @@ module HW3(CLK_50, KEY, SW, HEX0, HEX1, HEX2, HEX3, HEX4, HEX5, HEX6, HEX7, LEDR
 			out   = guess;
 		end
 		SEnd:begin
-			if(A == 4)
+			if(answer == guess)
 				out  = 16'b0000_0001_0010_0010; // PASS
 			else
 				out  = 16'b0100_0011_0010_0101; // LOSE
@@ -160,40 +172,42 @@ module HW3(CLK_50, KEY, SW, HEX0, HEX1, HEX2, HEX3, HEX4, HEX5, HEX6, HEX7, LEDR
 		case (State)
 			Set:begin
 				answer  = SW[15:0];
+				
 				guess  = 0;
-				answer  = 0;
+				
 				A   = 0;
 				B    = 0;
 				showA = 0;
 				showB = 0;
+				flag <= 0;
 			end
 			Waiting:begin
 				{A,showA,B,showB} =  {4{run}};
 			end
-			Play:begin    
-				if(~_KEY3)begin
-				// Judeg A
-				A = (guess[15:12] == answer[15:12]) ? A + 1 : A;
-				A = (guess[11:8]  == answer[11:8] ) ? A + 1 : A;
-				A = (guess[7:4]  == answer[7:4]  ) ? A + 1 : A;
-				A = (guess[3:0]  == answer[3:0]  ) ? A + 1 : A;
-				
-				// Judge B
-				B = (guess[15:12] == answer[11:8]  || guess[15:12]  == answer[7:4]  || guess[15:12] == answer[3:0] ) ? B + 1 : B;
-				B = (guess[11:8]  == answer[15:12]  || guess[11:8]  == answer[7:4]  || guess[11:8]  == answer[3:0] ) ? B + 1 : B;
-				B = (guess[7:4]  == answer[15:12]  || guess[7:4]   == answer[11:8]  || guess[7:4]   == answer[3:0] ) ? B + 1 : B;
-				B = (guess[3:0]  == answer[15:12]  || guess[3:0]   == answer[11:8]  || guess[3:0]   == answer[7:4] ) ? B + 1 : B;
+			Play:begin
+			
+				if(!_KEY3)begin
+					A = 0;
+					B = 0;
+					// Judeg A
+					if (guess[15:12] 	== answer[15:12])  A = A + 1;
+					if (guess[11:8]  	== answer[11:8] )  A = A + 1;
+					if (guess[7:4]  	== answer[7:4]  )  A = A + 1;
+					if (guess[3:0] 	== answer[3:0]  )  A = A + 1;
+					
+					// Judge B
+					if(guess[15:12] == answer[11:8] 	|| guess[15:12] 	== answer[7:4]  	|| guess[15:12] == answer[3:0] ) B = B + 1 ;
+					if(guess[11:8]  == answer[15:12]  || guess[11:8]  == answer[7:4]  	|| guess[11:8]  == answer[3:0] )B = B + 1 ;
+					if(guess[7:4]  	== answer[15:12]  || guess[7:4]   == answer[11:8]  || guess[7:4]   == answer[3:0] )B = B + 1 ;
+					if(guess[3:0]  	== answer[15:12]  || guess[3:0]   == answer[11:8]  || guess[3:0]   == answer[7:4] )B = B + 1 ;
 				end else if(_KEY3) begin
-				guess[15:0] = SW[15:0];
+					guess[15:0] = SW[15:0];
 				end 
 				showA  = 10;
 				showB  = 11;
 			end
 			SEnd:begin
-				A <= 6;// default: show nothing;s
-				B <= 6;// default: show nothing;
-				showA = 0;
-				showB = 0;
+				{A,showA,B,showB} =  {4{4'b0011}};
 			end
 		endcase
 	end
@@ -202,53 +216,53 @@ module HW3(CLK_50, KEY, SW, HEX0, HEX1, HEX2, HEX3, HEX4, HEX5, HEX6, HEX7, LEDR
 
 module LED_Decoder(data, out, mode);
 	// ===== Decode the input data to 7-LED
-	input   [3:0] data;
-	input   [1:0] mode;
+	input   		[3:0] data;
+	input   		[1:0] mode;
 	output reg  [6:0] out;
 	
 	always@(*)begin
-	if(mode == 0 || mode == 2)begin
-		case(data)
-			0 : out <= ~7'b0111111;
-			1 : out <= ~7'b0000110; 
-			2 : out <= ~7'b1011011;
-			3 : out <= ~7'b1001111;
-			4 : out <= ~7'b1100110;
-			5 : out <= ~7'b1101101;
-			6 : out <= ~7'b1111101;
-			7 : out <= ~7'b0000111;
-			8 : out <= ~7'b1111111;
-			9 : out <= ~7'b1101111;
-			10: out <= ~7'b1110111; // A
-			11: out <= ~7'b1111100; // b
-			12: out <= ~7'b1011000; // c
-			13: out <= ~7'b1011110; // d
-			14: out <= ~7'b1111001; // E
-			15: out <= ~7'b1110001; // F
-		endcase
-	end else if(mode == 3) begin  // PASS of LOSE
-		case(data)
-			0 : out <= ~7'b1110011; // P
-			1 : out <= ~7'b1110111; // A
-			2 : out <= ~7'b1101101; // S
-			3 : out <= ~7'b0111111; // O
-			4 : out <= ~7'b0111000; // L
-			5 : out <= ~7'b1111001; // E
-			default : out <= ~7'b0000000; // Waiting Mode
-		endcase
-	end else if (mode == 1) begin // Run Circle
-		case(data)
-			0 : out <= ~7'b0000001; // 0
-			1 : out <= ~7'b0000010; // 1
-			2 : out <= ~7'b0000100; // 2
-			3 : out <= ~7'b0001000; // 3
-			4 : out <= ~7'b0010000; // 4
-			5 : out <= ~7'b0100000; // 5
-			default : out <= ~7'b0000000; // Waiting Mode
-		endcase
+		if(mode == 0 || mode == 2)begin
+			case(data)
+				0 : out <= ~7'b0111111;
+				1 : out <= ~7'b0000110; 
+				2 : out <= ~7'b1011011;
+				3 : out <= ~7'b1001111;
+				4 : out <= ~7'b1100110;
+				5 : out <= ~7'b1101101;
+				6 : out <= ~7'b1111101;
+				7 : out <= ~7'b0000111;
+				8 : out <= ~7'b1111111;
+				9 : out <= ~7'b1101111;
+				10: out <= ~7'b1110111; // A
+				11: out <= ~7'b1111100; // b
+				12: out <= ~7'b1011000; // c
+				13: out <= ~7'b1011110; // d
+				14: out <= ~7'b1111001; // E
+				15: out <= ~7'b1110001; // F
+			endcase
+		end else if(mode == 3) begin  // PASS of LOSE
+			case(data)
+				0 : out <= ~7'b1110011; // P
+				1 : out <= ~7'b1110111; // A
+				2 : out <= ~7'b1101101; // S
+				3 : out <= ~7'b0111111; // O
+				4 : out <= ~7'b0111000; // L
+				5 : out <= ~7'b1111001; // E
+				default : out <= ~7'b0000000; // Waiting Mode
+			endcase
+		end else if (mode == 1) begin // Run Circle
+			case(data)
+				0 : out <= ~7'b0000001; // 0
+				1 : out <= ~7'b0000010; // 1
+				2 : out <= ~7'b0000100; // 2
+				3 : out <= ~7'b0001000; // 3
+				4 : out <= ~7'b0010000; // 4
+				5 : out <= ~7'b0100000; // 5
+				default : out <= ~7'b0000000; // Waiting Mode
+			endcase
+		end
 	end
-	end
-	endmodule
+endmodule
 	
 module Debounce(clk, in, out);
 	input    in, clk;
---
+++ 111-1/1124/HW3/output_files/HW3.cdf
@@ -0,0 +1,13 @@
+/* Quartus II 64-Bit Version 13.0.1 Build 232 06/12/2013 Service Pack 1 SJ Web Edition */
+JedecChain;
+	FileRevision(JESD32A);
+	DefaultMfr(6E);
+
+	P ActionCode(Cfg)
+		Device PartName(EP4CE115F29) Path("C:/Users/dht98/Downloads/NKUST/NKUST_Digital_System_Design_110/111-1/1124/HW3/output_files/") File("HW3.sof") MfrSpec(OpMask(1));
+
+ChainEnd;
+
+AlteraBegin;
+	ChainType(JTAG);
+AlteraEnd;
